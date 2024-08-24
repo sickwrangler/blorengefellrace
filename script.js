69d106dc63f6a46bc01d714be60600a5616dc641
sickwrangler <patrickj.jarvis@gmail.com>
2024-08-24 14:25:42 +0100

    added mroe information and buttons

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        window.scrollTo({
+            top: element.offsetTop - document.querySelector('header').offsetHeight + 350,
+            behavior: 'smooth'
+        });
+    }
+}
